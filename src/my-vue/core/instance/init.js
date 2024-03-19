4c287351ad99e36eec5587a045a47f4ce84a1ddd
wujiabao <wujiabao@leateckgroup.com>
2024-03-19 17:41:02 +0800

    第七节vm._render

--- src/my-vue/core/instance/init.js
+++ src/my-vue/core/instance/init.js
@@ -1,7 +1,11 @@
+import { initRender } from "./render";
+
 export function initMixin(Vue){
     Vue.prototype._init = function (options){
         const vm = this;
         
         vm.$options = options;
+        
+        initRender(vm);
     }
 }
